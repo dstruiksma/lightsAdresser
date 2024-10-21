87ba3bac71a4d610724967e661de11de5427e1a3
dstruiksma <detmer56@gmail.com>
2024-10-21 15:53:17 +0200

    added git ignore

---
+++ controller.py
@@ -0,0 +1,33 @@
+import board
+import neopixel
+from time import sleep
+
+
+def rgb_to_grb(color):
+    color[0], color[1] = color[1], color[0]
+    return color
+
+
+class LightsController:
+    def __init__(self):
+        self.lights = neopixel.NeoPixel(board.D18, 400, brightness=1, auto_write=False)
+        self.color = (255, 50, 10)
+
+    def set_light_color(self, color):
+        color = rgb_to_grb(color)
+        self.set_color(color)
+        self.lights.fill((color[0], color[1], color[2]))
+        self.lights.show()
+
+    def set_brightness(self, brightness):
+        self.lights.brightness = brightness
+        self.lights.show()
+
+    def turn_on(self):
+        self.lights.fill(self.color)
+
+    def turn_off(self):
+        self.lights.fill((0, 0, 0))
+
+    def set_color(self, color):
+        self.color = (color[0], color[1], color[2])
---
+++ lights_api.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+from controller import LightsController
+
+lights_controller = LightsController()
+app = Flask(__name__)
+
+@app.route('/set_color', methods=['POST'])
+def set_color():
+    data = request.json
+    color = data['color']
+    lights_controller.set_light_color(color)
+    return jsonify(message="color set")
+
+@app.route('/set_brightness', methods=['POST'])
+def set_brightness():
+    data = request.json
+    lights_controller.set_brightness(data['brightness'])
+    return jsonify(message="brightness set")
+
+@app.route('/turn_on', methods=['GET'])
+def turn_on():
+    lights_controller.turn_on()
+    return jsonify(message="turned on")
+
+@app.route('/turn_off', methods=['GET'])
+def turn_on():
+    lights_controller.turn_off()
+    return jsonify(message="turned ff")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
